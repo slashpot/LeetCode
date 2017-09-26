57888695fa22a28c9bb665f7e0148fddf8cafb0d
Stan Lin <ttttt123465@gmail.com>
2017-09-26 23:35:37 +0800

    Add Contain Duplicate II

---
+++ LeetCode/LeetCode/ContainsDuplicateII.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
+{
+    public class ContainsDuplicateII
+    {
+        public static bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+            var indexLookUp = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                indexLookUp.Add(i, nums[i]);
+            }
+
+            var duplicateNums = nums.GroupBy(x => x).Where(y => y.Count() >= 2).Select(x => x.Key).ToArray();
+            foreach (var num in duplicateNums)
+            {
+                var keys = indexLookUp.Where(x => x.Value.Equals(num)).Select(x => x.Key).ToArray();
+                for (int i = 0; i < keys.Length - 1; i++)
+                {
+                    if (keys[i + 1] - keys[i] <= k)
+                        return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTest/ContainsDuplicateIITest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace LeetCodeTest
+{
+    [TestFixture]
+    public class ContainsDuplicateIITest
+    {
+        [Test]
+        public void Empty_Array_Test()
+        {
+            var input = new int[] { };
+            Assert.IsFalse(LeetCode.ContainsDuplicateII.ContainsNearbyDuplicate(input, 0));
+        }
+
+        [TestCase(new[] {3,4,2,5,9,2}, 4)]
+        public void Normal_Array_Test(int[] input, int k)
+        {
+            Assert.IsTrue(LeetCode.ContainsDuplicateII.ContainsNearbyDuplicate(input, k));
+        }
+    }
+}
