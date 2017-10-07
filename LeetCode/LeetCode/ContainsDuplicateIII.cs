cc7d1762c1ced8de660b851a110688c9faefdf0e
Stan Lin <ttttt123465@gmail.com>
2017-10-07 22:52:37 +0800

    still writing ContainsDuplicateIII

---
+++ LeetCode/LeetCode/ContainsDuplicateIII.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LeetCode
+{
+    public class ContainsDuplicateIII
+    {
+        public bool ContainsNearbyAlmostDuplicate(int[] nums, int k, int t)
+        {
+            if (nums.Length == 0)
+                return false;
+
+            for (int i = 0; i < nums.Length; i += (k + 1))
+            {
+                for (int j = i+1; j < nums.Length && j - i <= k; j++)
+                {
+                    if (Math.Abs((decimal)nums[i] - nums[j]) <= t)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTest/ContainsDuplicateIIITest.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode;
+using NUnit.Framework;
+
+namespace LeetCodeTest
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestEmptyArray()
+        {
+            var containsDuplicate = new ContainsDuplicateIII();
+            var actual = containsDuplicate.ContainsNearbyAlmostDuplicate(new int[0], 0, 0);
+            Assert.AreEqual(false, actual);
+        }
+
+        [TestCase(new[] { -1, -1 }, 1, 0, true)]
+        [TestCase(new[] { -1, 2147483647 }, 1, 2147483647, false)]
+        [TestCase(new[] { 1, 3, 1 }, 2, 1, true)]
+        [TestCase(new[] { 1, 2 }, 0, 1, false)]
+        [TestCase(new[] { 7, 1, 3 }, 2, 3, true)]
+        public void TestNormalCases(int[] nums, int k, int t, bool expected)
+        {
+            var containsDuplicate = new ContainsDuplicateIII();
+            var actual = containsDuplicate.ContainsNearbyAlmostDuplicate(nums, k, t);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
