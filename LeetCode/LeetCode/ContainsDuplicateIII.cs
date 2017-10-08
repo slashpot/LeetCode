1779d76fb2d46d1b5503581dfe6ceeb3c132eca3
Stan Lin <ttttt123465@gmail.com>
2017-10-08 12:48:40 +0800

    finish ContainsDuplicateIII

--- LeetCode/LeetCode/ContainsDuplicateIII.cs
+++ LeetCode/LeetCode/ContainsDuplicateIII.cs
@@ -1,25 +1,115 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace LeetCode
 {
     public class ContainsDuplicateIII
     {
-        public bool ContainsNearbyAlmostDuplicate(int[] nums, int k, int t)
-        {
-            if (nums.Length == 0)
-                return false;
+        private int[] _nums;
+        private int _maxAbsOfIndex;
+        private int _maxAbsOfValue;
+        private HashSet<int> _searchedLookUp;
 
-            for (int i = 0; i < nums.Length; i += (k + 1))
+        public bool ContainsNearbyAlmostDuplicate(int[] nums, int maxAbsOfIndex, int maxAbsOfValue)
+        {
+            GetValuesForFields(nums, maxAbsOfIndex, maxAbsOfValue);
+
+            if (HasOneNumOrKEqualsToZero())
             {
-                for (int j = i+1; j < nums.Length && j - i <= k; j++)
+                return false;
+            }
+
+            return IsCheckEqual() ? HasDuplicates() : HasDuplicatesWithAbs();
+        }
+
+        private void GetValuesForFields(int[] nums, int maxAbsDiffOfIndex, int maxAbsDiffOfValue)
+        {
+            _nums = nums;
+            _maxAbsOfValue = maxAbsDiffOfValue;
+            _maxAbsOfIndex = maxAbsDiffOfIndex;
+            _searchedLookUp = new HashSet<int>();
+        }
+
+        private bool HasOneNumOrKEqualsToZero()
+        {
+            return _nums.Length <= 1 || _maxAbsOfIndex == 0;
+        }
+
+        private bool IsCheckEqual()
+        {
+            return _maxAbsOfValue == 0;
+        }
+
+        private bool HasDuplicates()
+        {
+            for (int i = 0; i < _nums.Length; i++)
+            {
+                if (FindDuplicateInLookUp(i))
                 {
-                    if (Math.Abs((decimal)nums[i] - nums[j]) <= t)
-                    {
-                        return true;
-                    }
+                    return true;
                 }
+
+                AddToLookUp(i);
+                RemoveOutOfWindowNumFromLookUp(i);
             }
             return false;
+        }
+
+        private bool FindDuplicateInLookUp(int i)
+        {
+            return _searchedLookUp.Contains(_nums[i]);
+        }
+
+        private void AddToLookUp(int i)
+        {
+            _searchedLookUp.Add(_nums[i]);
+        }
+
+        private void RemoveOutOfWindowNumFromLookUp(int i)
+        {
+            if (i >= _maxAbsOfIndex)
+            {
+                _searchedLookUp.Remove(_nums[i - _maxAbsOfIndex]);
+            }
+        }
+
+        private bool HasDuplicatesWithAbs()
+        {
+            for (int i = 0; i < _nums.Length; i++)
+            {
+                var highest = GetHighest(i);
+                var lowest = GetLowest(i);
+
+                var matchNums = GetNumsInRange(highest, lowest);
+                if (HasMatch(matchNums))
+                {
+                    return true;
+                }
+
+                AddToLookUp(i);
+                RemoveOutOfWindowNumFromLookUp(i);
+            }
+            return false;
+        }
+
+        private decimal GetHighest(int i)
+        {
+            return (decimal)_nums[i] + _maxAbsOfValue;
+        }
+
+        private decimal GetLowest(int i)
+        {
+            return (decimal)_nums[i] - _maxAbsOfValue;
+        }
+
+        private IEnumerable<int> GetNumsInRange(decimal highest, decimal lowest)
+        {
+            return _searchedLookUp.Where(x => (x <= highest && x >= lowest));
+        }
+
+        private static bool HasMatch(IEnumerable<int> matchNums)
+        {
+            return matchNums.Count() != 0;
         }
     }
 }
--- LeetCode/LeetCodeTest/ContainsDuplicateIIITest.cs
+++ LeetCode/LeetCodeTest/ContainsDuplicateIIITest.cs
@@ -6,24 +6,41 @@ namespace LeetCodeTest
     [TestFixture]
     public class UnitTest1
     {
-        [Test]
-        public void TestEmptyArray()
-        {
-            var containsDuplicate = new ContainsDuplicateIII();
-            var actual = containsDuplicate.ContainsNearbyAlmostDuplicate(new int[0], 0, 0);
-            Assert.AreEqual(false, actual);
-        }
-
-        [TestCase(new[] { -1, -1 }, 1, 0, true)]
-        [TestCase(new[] { -1, 2147483647 }, 1, 2147483647, false)]
-        [TestCase(new[] { 1, 3, 1 }, 2, 1, true)]
-        [TestCase(new[] { 1, 2 }, 0, 1, false)]
-        [TestCase(new[] { 7, 1, 3 }, 2, 3, true)]
-        public void TestNormalCases(int[] nums, int k, int t, bool expected)
+        private static void TestContainsDuplicate(int[] nums, int k, int t, bool expected)
         {
             var containsDuplicate = new ContainsDuplicateIII();
             var actual = containsDuplicate.ContainsNearbyAlmostDuplicate(nums, k, t);
             Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new int[0] , 0, 0, false)]
+        public void TestEmptyArray(int[] nums, int k, int t, bool expected)
+        {
+            TestContainsDuplicate(nums, k, t, expected);
+        }
+
+        [TestCase(new[] { 1 }, 1, 1, false)]
+        public void TestOneNumCases(int[] nums, int k, int t, bool expected)
+        {
+            TestContainsDuplicate(nums, k, t, expected);
+        }
+
+        [TestCase(new[] { -1, -1 }, 1, 0, true)]
+        [TestCase(new[] { 1, 3, 1 }, 2, 1, true)]
+        [TestCase(new[] { 1, 3, 1 }, 1, 1, false)]
+        [TestCase(new[] { 1, 2 }, 0, 1, false)]
+        [TestCase(new[] { 7, 1, 3 }, 2, 3, true)]
+        [TestCase(new[] { -3, 3 }, 2, 4, false)]
+        public void TestNormalCases(int[] nums, int k, int t, bool expected)
+        {
+            TestContainsDuplicate(nums, k, t, expected);
+        }
+
+        [TestCase(new[] { -1, 2147483647 }, 1, 2147483647, false)]
+        [TestCase(new[] { 0, 2147483647 }, 1, 2147483647, true)]
+        public void TestIntMaxValueCases(int[] nums, int k, int t, bool expected)
+        {
+            TestContainsDuplicate(nums, k, t, expected);
         }
     }
 }
